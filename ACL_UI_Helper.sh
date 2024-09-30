13d4e4132f0eba01c45b53652905cc0be6e5fd9d
VPJoeM <jmannix@voltagepark.com>
2024-09-30 20:34:45 +0100

    Update ACL_UI_Helper.sh

--- ACL_UI_Helper.sh
+++ ACL_UI_Helper.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Filename: ACL_UI_Help.sh
 
-# Prompt the user to paste the input and then press Ctrl+D to end input
 echo "Please paste the input and then press Ctrl+D to finish:"
 
-# Read all input into a variable
 input=$(</dev/stdin)
 
-# Extract and print numbers following 'g' in hostnames only
 echo "$input" | grep -o 'g[0-9]\{1,\}\.voltagepark\.net' | sed -E 's/g([0-9]+)\.voltagepark\.net/\1/'
